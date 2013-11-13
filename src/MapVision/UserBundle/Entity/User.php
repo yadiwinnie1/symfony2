6657de366657c920529a6f0cbe2263ba4aee4dc1
mekontsoaris <mekontsoaris@gmail.com>
2013-11-13 03:37:30 -0500

    Gestion des utilisateurs effectuée
    Il reste le design des formulaires de cette gestion

---
+++ src/MapVision/UserBundle/Entity/User.php
@@ -0,0 +1,20 @@
+<?php
+// src/MapVision/UserBundle/Entity/User.php
+namespace MapVision\UserBundle\Entity;
+use FOS\UserBundle\Model\User as BaseUser; 
+use Doctrine\ORM\Mapping as ORM;
+/**
+  * @ORM\Entity
+  * @ORM\Table(name="user")
+  */
+class User extends BaseUser{
+/**
+  * @ORM\Id
+  *	@ORM\Column(type="integer")
+  * @ORM\GeneratedValue(strategy="AUTO")
+  */
+  protected $id;
+  public function __construct(){
+		parent::__construct();
+  }
+}
---
+++ src/MapVision/UserBundle/MapVisionUserBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace MapVision\UserBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class MapVisionUserBundle extends Bundle
+{
+}
